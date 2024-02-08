c6dafcd0837e6446968df4474014343e0631a609
matheus1714 <matheussilvamartins1714@gmail.com>
2024-02-07 21:54:17 -0300

    config deploy story book

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -15,5 +15,11 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if(configType ==- 'PRODUCTION'){
+      config.base = '/ignite-lab-design-system/'
+    }
+    return config
+  }
 };
 export default config;
